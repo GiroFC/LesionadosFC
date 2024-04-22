d5ca9e0ad6616ab7edacc1bf591cf59167aaefb0
Igor Franco <igoretofranco@gmail.com>
2024-04-21 23:51:43 -0300

    Style - AddJogador, EditJogador e IndexJogador

--- LesionadosFC/src/app/tabela/tabela.component.ts
+++ LesionadosFC/src/app/tabela/tabela.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { IJogador } from 'src/models/jogador.model';
 
+
 @Component({
   selector: 'app-tabela',
   templateUrl: './tabela.component.html',
