3ce5e8fc9ceafec025c25c093ef127532bc0ef76
MatheusFortes7 <mfortesv@gmail.com>
2024-06-05 14:02:37 -0300

    melhorando ordem de listagem de jogadores

--- LesionadosFC/src/app/jogador-index/jogador-index.page.ts
+++ LesionadosFC/src/app/jogador-index/jogador-index.page.ts
@@ -30,6 +30,10 @@ export class JogadorIndexPage {
 
   public listarJogadores() {
     this.jogadorServ.getAll().subscribe((jogadores: IJogador[]) => {
+      //quero ordenar os jogadores por ordem alfabetica
+      jogadores.sort((a, b) => {
+        return a.nome.localeCompare(b.nome);
+      });
       this.jogadores = jogadores;
     });
   }
